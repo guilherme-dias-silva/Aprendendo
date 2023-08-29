6836db228a8c4e86ef91b6dda573ced1026de692
guilherme-dias-silva <129855809+guilherme-dias-silva@users.noreply.github.com>
2023-08-29 19:19:55 -0300

    Goto
    Testando goto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto2;
+
+public class Programa2{
+    
+    static void Main(string[] args){
+
+
+    int bola = 1;
+
+        Looop:
+
+        Console.WriteLine($"Bola numero {bola}");
+    
+        if(bola<11){
+        bola++;
+        goto Looop;}
+    
+        Console.ReadKey();
+}
+}
