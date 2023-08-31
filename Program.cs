dbc796549e269d87112f3c85e6d817e9c8ec9771
guilherme-dias-silva <129855809+guilherme-dias-silva@users.noreply.github.com>
2023-08-31 11:02:39 -0300

    Add files via upload
    Aprendendo a usar While

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 
-namespace Projeto2;
+namespace std;
 
-public class Programa2{
-    
+public class Program{
+
     static void Main(string[] args){
+   
+    int numero;
+    int contador = 1;
+    Console.WriteLine("Digite um numero maior que zero");
+    numero = Convert.ToInt32(Console.ReadLine());
 
+if(numero > 0)
+{
+  Console.WriteLine($"\n## Tabuada do {numero} ##\n");
+  while(contador <= 10)
+  {
+    Console.WriteLine($"{numero} x {contador} = {numero*contador}");
+    contador++;
+  }
+}
+else
+{
+   Console.WriteLine("Digite um numero maior que zero!");
+}
 
-    int bola = 1;
-
-        Looop:
-
-        Console.WriteLine($"Bola numero {bola}");
-    
-        if(bola<11){
-        bola++;
-        goto Looop;}
-    
-        Console.ReadKey();
+Console.ReadKey();
 }
 }
