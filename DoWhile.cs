a4269d52247d7a0dc79c37d85200d4d3d4aa28f2
guilherme-dias-silva <129855809+guilherme-dias-silva@users.noreply.github.com>
2023-09-07 11:12:49 -0300

    Add files via upload
    dando uma olhada no do while

---
+++ DoWhile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoWhile;
+
+public class DoWhile{
+
+    static void Main(string[] args){
+
+    int x = 0;
+
+    do
+    {
+        int y = 0;
+    do
+    {
+        Console.Write($"({x},{y})");
+        y++;
+    }
+    while( y < 5);
+    x++;
+    Console.WriteLine();
+    }
+    while(x < 5);
+
+    Console.ReadKey();
+    
+
+}
+}
