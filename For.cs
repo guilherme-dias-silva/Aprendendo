73cf92be68fd89dbd172990bd36318bddb3d8d38
guilherme-dias-silva <129855809+guilherme-dias-silva@users.noreply.github.com>
2023-09-08 11:18:28 -0300

    Add files via upload
    Usando o loop for

---
+++ For.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace std;
+
+public class For{
+
+    static void Main(string[] args){
+
+    int resultado;
+    int i;
+    int numero;
+    System.Console.WriteLine("Informe um numero inteiro");
+    numero =  Convert.ToInt32(Console.ReadLine());
+
+
+    for(i=1; i<=10; i++)
+    {
+        resultado = i * numero;
+        System.Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+    Console.ReadKey();
+}
+}
