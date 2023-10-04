8ff8c55029c78018a5a55f00bd95829f0cd1130f
guilherme-dias-silva <129855809+guilherme-dias-silva@users.noreply.github.com>
2023-10-04 20:31:01 -0300

    Add files via upload
    Usando switch

---
+++ ProgramadePArcelas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace std;
+
+class Program{
+
+    static void Main(){
+
+    System.Console.WriteLine("Calculadora de parcelas");
+    int numeroParcelas = Convert.ToInt32(Console.ReadLine());
+    int valor = 600;
+
+    switch(numeroParcelas){
+    
+    case 1:
+    Console.WriteLine($"Valor da parcela é {valor / numeroParcelas}");
+    break;
+
+    case 2:
+    Console.WriteLine($"Valor da parcela é {valor / numeroParcelas}");
+    break;
+
+    case 3:
+    Console.WriteLine($"Valor da parcela é {valor / numeroParcelas}");
+    break;
+
+    case 4:
+    Console.WriteLine($"Valor da parcela é {valor / numeroParcelas}");
+    break;
+
+    default:
+    Console.WriteLine("Digite uma parcela existente");
+    break;
+    }
+    Console.ReadKey();
+    }
+}
