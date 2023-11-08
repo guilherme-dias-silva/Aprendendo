f097d13c8a9156ba0d104b2bbb502a69028d9583
guilherme-dias-silva <129855809+guilherme-dias-silva@users.noreply.github.com>
2023-11-08 13:39:33 -0300

    Add files via upload

---
+++ POOtres.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Escolha uma peça do carro");
+System.Console.WriteLine($"{Carro.Motor} - {(int)Carro.Motor}");
+System.Console.WriteLine($"{Carro.Roda} - {(int)Carro.Roda}");
+System.Console.WriteLine($"{Carro.Aro} - {(int)Carro.Aro}");
+System.Console.WriteLine($"{Carro.Freio} - {(int)Carro.Freio}");
+
+int valor = Convert.ToInt32(Console.ReadLine());
+
+var chave = (Carro)valor;
+Console.WriteLine($"Voce selecionou a categoria {chave.ToString()}");
+
+Console.ReadKey();
+
+enum Carro
+{
+    Motor,
+    Roda,
+    Aro,
+    Freio
+}
